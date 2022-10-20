f1510162e057d468310db5b7c023076e57f39231
Adam Lu <lym8356@gmail.com>
2022-10-20 18:31:13 +1100

    Cleaned up database

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
